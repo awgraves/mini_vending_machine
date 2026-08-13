3fa77db12d32386138a9590abf8b7e3fc04d7a96
Andrew Organist <aorganist@protonmail.com>
2026-08-13 16:09:09 -0400

    add second joystick axis ADC read

--- apps/gantry/src/main.c
+++ apps/gantry/src/main.c
@@ -1,9 +1,3 @@
-/*
- * Copyright (c) 2016 Intel Corporation
- *
- * SPDX-License-Identifier: Apache-2.0
- */
-
 #include <stdio.h>
 #include <zephyr/drivers/adc.h>
 #include <zephyr/kernel.h>
@@ -12,26 +6,47 @@
 
 #define JOYSTICK DT_ALIAS(my_joystick)
 
-static const struct adc_dt_spec my_adc_spec = ADC_DT_SPEC_GET(JOYSTICK);
+static const struct adc_dt_spec adc_channels[2] = {
+    ADC_DT_SPEC_GET_BY_IDX(JOYSTICK, 0), ADC_DT_SPEC_GET_BY_IDX(JOYSTICK, 1)};
+
+typedef union {
+  struct {
+    uint16_t x, y;
+  };
+  uint16_t raw[2];
+} readings_t; // each reading is 12 bits
 
 int main(void) {
   int ret;
+  readings_t readings = {0};
 
-  uint32_t buf = 0;
-  struct adc_sequence seq = {.buffer = &buf, .buffer_size = sizeof(buf)};
+  for (int i = 0; i < 2; i++) {
+    if (!adc_is_ready_dt(&adc_channels[i])) {
+      return 0;
+    }
 
-  ret = adc_sequence_init_dt(&my_adc_spec, &seq);
-  if (ret < 0) {
-    return 0;
+    if (adc_channel_setup_dt(&adc_channels[i]) < 0) {
+      return 0;
+    }
   }
+  struct adc_sequence seq = {.buffer_size = sizeof(uint32_t)};
 
   while (1) {
-    ret = adc_read_dt(&my_adc_spec, &seq);
-    if (ret < 0) {
-      printf("Could not read ADC: %d\n", ret);
-    } else {
-      printf("ADC VAL: %d\n", buf);
+    for (int i = 0; i < 2; i++) {
+      seq.buffer = &readings.raw[i];
+
+      ret = adc_sequence_init_dt(&adc_channels[i], &seq);
+      if (ret < 0) {
+        return 0;
+      }
+
+      ret = adc_read_dt(&adc_channels[i], &seq);
+      if (ret < 0) {
+        printf("FAILED to read ADC %s\n", adc_channels[i].dev->name);
+      }
     }
+
+    printf("ADC VALs: X=%d Y=%d\n", readings.x, readings.y);
 
     k_msleep(SLEEP_TIME_MS);
   }
