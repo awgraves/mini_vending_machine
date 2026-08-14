df71c2716dd2a57d98d99efaf5071c17e41d32ad
Andrew Organist <aorganist@protonmail.com>
2026-08-14 17:05:15 -0400

    add pwm leds to indicate joystick adc readings

--- apps/gantry/src/main.c
+++ apps/gantry/src/main.c
@@ -1,13 +1,20 @@
 #include <stdio.h>
 #include <zephyr/drivers/adc.h>
+#include <zephyr/drivers/led.h>
 #include <zephyr/kernel.h>
 
 #define SLEEP_TIME_MS 20
+#define MAX_ADC_READING 4095
 
 #define JOYSTICK DT_ALIAS(my_joystick)
+#define RED_LED_NODE DT_ALIAS(red_pwm_led)
+#define YELLOW_LED_NODE DT_ALIAS(yellow_pwm_led)
 
 static const struct adc_dt_spec adc_channels[2] = {
     ADC_DT_SPEC_GET_BY_IDX(JOYSTICK, 0), ADC_DT_SPEC_GET_BY_IDX(JOYSTICK, 1)};
+
+static const struct led_dt_spec red_led = LED_DT_SPEC_GET(RED_LED_NODE);
+static const struct led_dt_spec yellow_led = LED_DT_SPEC_GET(YELLOW_LED_NODE);
 
 typedef union {
   struct {
@@ -19,6 +26,10 @@ typedef union {
 int main(void) {
   int ret;
   readings_t readings = {0};
+
+  if (!device_is_ready(red_led.dev) || !device_is_ready(yellow_led.dev)) {
+    return 0;
+  }
 
   for (int i = 0; i < 2; i++) {
     if (!adc_is_ready_dt(&adc_channels[i])) {
@@ -47,6 +58,9 @@ int main(void) {
     }
 
     printf("ADC VALs: X=%d Y=%d\n", readings.x, readings.y);
+
+    led_set_brightness_dt(&red_led, readings.x * 100 / MAX_ADC_READING);
+    led_set_brightness_dt(&yellow_led, readings.y * 100 / MAX_ADC_READING);
 
     k_msleep(SLEEP_TIME_MS);
   }
